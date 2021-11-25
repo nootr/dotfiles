35db6cd3588aa308ed0833b8ee7c7f9816afbc8e
Joris Hartog <jorishartog@hotmail.com>
2021-11-25 21:47:29 +0100

    Add fish config to enable vim mode

---
+++ bin/.config/fish/config.fish
@@ -0,0 +1,9 @@
+# Enable vim mode using "jj" as an escape.
+if status is-interactive
+  fish_vi_mode
+  function my_vi_bindings
+    fish_vi_key_bindings
+    bind -M insert -m default jj backward-char force-repaint
+  end
+  set -g fish_key_bindings my_vi_bindings
+end
