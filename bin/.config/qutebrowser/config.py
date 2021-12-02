3c8eef652daebb97870e17e2d5385269d9549bdb
Joris Hartog <jorishartog@hotmail.com>
2021-12-02 20:30:37 +0100

    Add qutebrowser config

---
+++ bin/.config/qutebrowser/config.py
@@ -0,0 +1,4 @@
+config.load_autoconfig(True)
+c.url.start_pages = ["https://github.com/"]
+c.url.default_page = "https://google.com/"
+c.url.searchengines = {"DEFAULT": "https://google.com/search?q={}"}
